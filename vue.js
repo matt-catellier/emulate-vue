d8b7eeb0101da5eae8695f4c02383158b637cd56
Matt Catellier <matt@techsamurais.com>
2021-09-07 20:59:30 -0700

    Replace based on template escape characters

--- vue.js
+++ vue.js
@@ -1,6 +1,13 @@
 class Vue {
   constructor(options) {
     const app = document.querySelector(options.el)
-    app.innerHTML = options.data.message
+    const originalHtml = app.innerHTML
+
+    let newHtml = ''
+    Object.entries(options.data).forEach(([key, value]) => { 
+      newHtml = originalHtml.replace(`{{ ${key} }}`, value) 
+    })
+
+    app.innerHTML = newHtml
   }
 }
