c9c97eaf30f2cf93363ed68125b435041d8e2f58
Matt Catellier <matt@techsamurais.com>
2021-09-07 21:29:13 -0700

    TTraverse tall elements in dom, checking all TextNodes for data to replace

--- vue.js
+++ vue.js
@@ -1,13 +1,31 @@
 class Vue {
   constructor(options) {
-    const app = document.querySelector(options.el)
-    const originalHtml = app.innerHTML
+    this.root = document.querySelector(options.el)
+    this.data = options.data
+    
+    this.render()
+  }
 
-    let newHtml = ''
-    Object.entries(options.data).forEach(([key, value]) => { 
-      newHtml = originalHtml.replace(`{{ ${key} }}`, value) 
-    })
+  render() {
+    // start at root 
+    const stack = [this.root]
+    while(stack.length) {
+      console.log(stack)
+      // take dom element off stack
+      let element = stack.pop()
+      // add all its children to the stack
+      if(element.hasChildNodes()) {
+        stack.push(...element.childNodes)
+      }
 
-    app.innerHTML = newHtml
+      // if the element is a text node, then replace contents with data
+      if(element.nodeType === Node.TEXT_NODE) {
+        let newTextContent = ''
+        Object.entries(this.data).forEach(([key, value]) => { 
+          newTextContent = element.textContent.replace(new RegExp(`{{ ${key} }}`, "g"), value) 
+        })
+        element.textContent = newTextContent
+      }
+    }
   }
 }
